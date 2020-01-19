50a49907e096ab509f7a3e6a3b8a27f875917238
Robert Au <19robertau132@gmail.com>
2020-01-19 04:14:54 -0800

    added logo

--- script.js
+++ script.js
@@ -10,5 +10,13 @@ if (document.getElementById("english")){
 if (document.getElementById("programming")){
     document.body.style.backgroundColor = "rgb(65, 95, 130)"; 
 }
-
+//add logo
+const img = document.createElement("IMG");
+img.setAttribute("src", "steveGeneral.png");
+img.setAttribute("width", "300px");
+img.setAttribute("height", "300px");
+img.style.display = "block";
+img.style.marginLeft = "auto";
+img.style.marginRight = "auto";
+document.getElementById("steve").appendChild(img);
 const menu = document.getElementById("dropdown");
