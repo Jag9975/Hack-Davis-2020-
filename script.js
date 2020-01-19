f0c16df0b618daec88d75db4025656c3ace21c18
Robert Au <19robertau132@gmail.com>
2020-01-19 02:51:01 -0800

    Created a series of pages that can be more specific

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-const searchbox = document.getElementsByClassName("gsc-results-wrapper-overlay");
+if (document.getElementById("general")){
+    document.body.style.backgroundColor = "burlywood"; 
+}
+if (document.getElementById("math")){
+    document.body.style.backgroundColor = "rgb(185, 55, 44)"; 
+}
+if (document.getElementById("english")){
+    document.body.style.backgroundColor = "rgb(250, 220, 111)"; 
+}
+if (document.getElementById("programming")){
+    document.body.style.backgroundColor = "rgb(65, 95, 130)"; 
+}
+
+const menu = document.getElementById("dropdown");
